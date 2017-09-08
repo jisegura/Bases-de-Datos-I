2a7c5023b1d7a36e92088b688caf48248aa14f22
jisegura <segurajuanignacio@outlook.com>
2017-09-08 20:31:54 -0300

    add P02E01f

---
+++ Practico 02/Parte 2/P02E01f.sql
@@ -0,0 +1,15 @@
+SELECT p.nombre_pais, d.cantidad_voluntario AS "voluntarios mayores de edad"
+FROM unc_esq_voluntario.pais p
+INNER JOIN (SELECT d.id_pais, SUM(COALESCE(i.cantidad_voluntario,0)) AS "cantidad_voluntario"
+			FROM unc_esq_voluntario.direccion d
+			LEFT JOIN (SELECT i.id_institucion, i.id_direccion, COUNT(v.nro_voluntario) AS "cantidad_voluntario"
+						FROM unc_esq_voluntario.institucion i
+						INNER JOIN unc_esq_voluntario.voluntario v
+								ON i.id_institucion = v.id_institucion
+						WHERE v.fecha_nacimiento < now() - interval '21 year'
+						GROUP BY i.id_institucion, i.id_direccion
+						HAVING COUNT(v.nro_voluntario) > 4
+						   AND MAX(v.horas_aportadas) - MIN(v.horas_aportadas) < 5000) i
+			       ON d.id_direccion = i.id_direccion
+			GROUP BY d.id_pais) d
+        ON p.id_pais = d.id_pais;
