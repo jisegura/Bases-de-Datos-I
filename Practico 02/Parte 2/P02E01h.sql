c995e9cc15e7bf6efe700be601c2fd51c1fc8a10
jisegura <segurajuanignacio@outlook.com>
2017-09-09 17:39:25 -0300

    update P02E01h

--- Practico 02/Parte 2/P02E01h.sql
+++ Practico 02/Parte 2/P02E01h.sql
@@ -1,3 +1,4 @@
+-- Opcion 1
 SELECT v.nro_voluntario, v.nombre, v.apellido
 FROM unc_esq_voluntario.voluntario v
 INNER JOIN (
@@ -18,4 +19,29 @@ INNER JOIN (
 WHERE v.nro_voluntario IN (
 	SELECT id_director
 	FROM unc_esq_voluntario.institucion
-	WHERE id_director IS NOT NULL);
+	WHERE id_director IS NOT NULL)
+ORDER BY v.nombre, v.apellido;
+
+-- Opcion 2
+SELECT v.nro_voluntario, v.nombre, v.apellido
+FROM unc_esq_voluntario.voluntario v
+INNER JOIN (
+	SELECT i.id_institucion
+	FROM unc_esq_voluntario.institucion i
+	INNER JOIN (
+		SELECT d.id_direccion
+		FROM unc_esq_voluntario.direccion d
+		INNER JOIN (
+			SELECT p.id_pais
+			FROM unc_esq_voluntario.pais p
+			INNER JOIN unc_esq_voluntario.continente c
+				    ON p.id_continente = c.id_continente
+			WHERE c.nombre_continente = 'Europeo') p
+			    ON d.id_pais = p.id_pais) d
+		    ON i.id_direccion = d.id_direccion) i
+	    ON v.id_institucion = i.id_institucion
+WHERE EXISTS (
+	SELECT *
+	FROM unc_esq_voluntario.institucion
+	WHERE id_director = v.nro_voluntario)
+ORDER BY v.nombre, v.apellido;
